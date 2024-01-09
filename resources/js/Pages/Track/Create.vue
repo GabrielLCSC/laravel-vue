29190af433ae654797405968f091506c23ce1aeb
Dautreppe Gabriel <gabriel.dautreppe@gmail.com>
2024-01-09 09:23:00 +0100

    first commit

---
+++ resources/js/Pages/Track/Create.vue
@@ -0,0 +1,25 @@
+<template>
+    <MusicLayout>
+        <template #title> Musiques </template>
+
+        <template #action>
+            <Link :href="route('tracks.index')"> Retour </Link>
+        </template>
+        <template #content> </template>
+    </MusicLayout>
+</template>
+
+<script>
+import { Link } from "@inertiajs/vue3";
+
+import MusicLayout from "@/Layouts/MusicLayout.vue";
+
+export default {
+    components: {
+        MusicLayout,
+        Link,
+    },
+    props: {},
+};
+</script>
+<style></style>
